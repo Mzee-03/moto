7ff02d29cc81eb8f8d25f14d8145a24b9326d53d
Mzee-03 <zouheirmohammed29@gmail.com>
2025-05-12 10:23:46 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins:[ react() ],
+  server:{
+    proxy:{
+      // forward /sanctum/* to Laravel
+      '/sanctum': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+      // forward your API calls too
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
+});
